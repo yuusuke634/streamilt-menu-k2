2c8b294ff2995990be993c0a8cb25e7b92bf59c8
gare <haradaforintern@gmail.com>
2025-06-18 15:11:19 +0900

    8st commit

--- sidebar.py
+++ sidebar.py
@@ -5,7 +5,7 @@ def show_sidebar():
     st.sidebar.title("メニュー")
     selected_option = st.sidebar.radio(
         "機能を選択してください:",
-        ("レシート読取(開発中)","食材リスト管理", "献立提案"),
+        ("食材リスト管理", "献立提案","レシート読取(開発中)"),
         key="main_menu_radio" # サイドバーのラジオボタンにもキーを付ける
     )
     return selected_option
